83b45b5af8dfc317433a3ac8c3f4780802752c82
Yoonton <mak-sim-2005@mail.ru>
2025-10-23 21:18:05 +0400

    govno

--- src/main/java/com/hakaton/tkp/Config/WebClientConfig.java
+++ src/main/java/com/hakaton/tkp/Config/WebClientConfig.java
@@ -1,14 +1,8 @@
 package com.hakaton.tkp.Config;
 
-import org.springframework.ai.chroma.vectorstore.ChromaApi;
-import org.springframework.ai.chroma.vectorstore.ChromaVectorStore;
-import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -21,7 +15,6 @@ public class WebClientConfig {
     public WebClient chromaWebClient() {
         return WebClient.builder()
                 .baseUrl(chromaUrl)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
