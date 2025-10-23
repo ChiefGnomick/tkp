0d49e92d7bb16864b173996e79310d188ab3086d
ChiefGnomick <chiefgnomick@gmail.com>
2025-10-23 13:20:37 +0400

    first commit

---
+++ src/main/java/com/hakaton/tkp/TkpApplication.java
@@ -0,0 +1,13 @@
+package com.hakaton.tkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakaton/tkp/TkpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakaton.tkp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
