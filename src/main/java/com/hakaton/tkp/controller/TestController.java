9facefeecc2b57178ca9324ea5b3f93964314e7c
ChiefGnomick <chiefgnomick@gmail.com>
2025-10-24 10:28:58 +0400

    fix

---
+++ src/main/java/com/hakaton/tkp/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.hakaton.tkp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hakaton.tkp.service.ChatService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController("/api")
+@RequiredArgsConstructor
+public class TestController {
+
+    private final ChatService chatService;
+
+    @GetMapping("/send")
+    public String getMethodName(@RequestParam String text) {
+        return chatService.sendPromptWithRAG(text);
+    }
+
+}
